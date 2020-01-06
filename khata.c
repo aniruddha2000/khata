98f6019f301145f6cba920d7204734e0368f79a3
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-01-06 21:28:50 +0530

    Add display keypress feature

--- khata.c
+++ khata.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -24,7 +26,11 @@ int main() {
 
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-        return 0;
+        if (iscntrl(c)) {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c)\n", c, c);
+        }
     }
-
+    return 0;
 }
