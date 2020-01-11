22f6571471235e83bbc3a8716c7aeecaba66cb23
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-01-11 11:33:42 +0530

    Add moving left at the start of a line

--- khata.c
+++ khata.c
@@ -331,6 +331,9 @@ void editorMoveCursor(int key) {
         case ARROW_LEFT:
             if (E.cx != 0) {
                 E.cx--;
+            } esle if (E.cy > 0) {
+                E.cy--;
+                E.cx = E.row[E.cy].size;
             }
             break;
         case ARROW_RIGHT:
@@ -348,6 +351,12 @@ void editorMoveCursor(int key) {
                 E.cy++;
             }
             break;
+    }
+
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row->size : 0;
+    if (E.cx > rowlen) {
+        E.cx = rowlen;
     }
 }
 
